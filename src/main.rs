72068de259611400a581c9a6189278f7c106fc53
Isaac Park <isaac@academia.edu>
2020-11-16 21:40:53 -0800

    Continue past some errors

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,24 @@ mod snodas;
 fn main() -> snodas::Result<()> {
 
     let mut connection = snodas::Connection::new()?;
-
+    let mut failures_in_row = 0;
     for date in snodas::days_with_data() {
         if snodas::filename_for_date(date,"data").exists() {
-            println!("Skipping already downloaded {:?}", date);
-
+            //println!("Skipping already downloaded {:?}", date);
+            failures_in_row=0;
             continue;
         }
-        println!("Download for {:?}", date);
-        connection.archive_date(date, "data")?;
+        println!("Downloading for {:?}", date);
+        if let Err(e) = connection.archive_date(date, "data") {
+            if failures_in_row > 5 {
+                return Err(e)
+            }
+
+            println!("Error for {:?}: {}", date, e);
+            failures_in_row+=1;
+        } else {
+            failures_in_row=0;
+        }
     }
     return Ok(());
 }
